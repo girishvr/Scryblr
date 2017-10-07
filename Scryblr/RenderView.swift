abe6545b273fda36f0e45937d3637aa71056fdfc
girishvr <girish.vin.r@gmail.com>
2017-10-07 13:56:36 +0530

    First commit

---
+++ Scryblr/RenderView.swift
@@ -0,0 +1,44 @@
+//
+//  RenderView.swift
+//  Scryblr
+//
+//  Created by Girish Rathod on 07/10/17.
+//  Copyright © 2017 Girish Rathod. All rights reserved.
+//
+
+import UIKit
+
+class RenderView:UIView {
+    
+    var pointsToDraw:[Int] = [] {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.backgroundColor = .clear
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        //context!.clear(self.bounds)
+        context!.setLineWidth(10.0)
+        
+        
+        if pointsToDraw.count > 4 {
+            
+            context?.move(to: CGPoint(x: CGFloat(pointsToDraw[0]), y: CGFloat(pointsToDraw[1])))
+            
+            for i in 2..<pointsToDraw.count {
+                if i % 2 == 0 {
+                    context?.addLine(to: CGPoint(x: CGFloat(pointsToDraw[i]), y: CGFloat(pointsToDraw[i + 1])))
+                }
+            }
+        }
+        
+        // Draw
+        context!.strokePath();
+    }
+}
---
+++ Scryblr/ViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  Scryblr
+//
+//  Created by Girish Rathod on 24/06/17.
+//  Copyright © 2017 Girish Rathod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var rawPoints:[Int] = []
+    var recognizer:DBPathRecognizer?
+    
+    @IBOutlet var renderView: RenderView!
+    
+    @IBOutlet var letterLabel: UILabel!
+    
+    var timer: Timer!
+    
+    override func viewDidLoad() {
+        
+        //define the number of direction of PathModel
+        let recognizer = DBPathRecognizer(sliceCount: 8, deltaMove: 16.0)
+        
+        //define specific formes to draw on PathModel
+        recognizer.addModel(PathModel(directions: [7, 1], datas:"A" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2,6,0,1,2,3,4,0,1,2,3,4], datas:"B" as AnyObject))
+        recognizer.addModel(PathModel(directions: [4,3,2,1,0], datas:"C" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2,6,7,0,1,2,3,4], datas:"D" as AnyObject))
+        recognizer.addModel(PathModel(directions: [4,3,2,1,0,4,3,2,1,0], datas:"E" as AnyObject))
+        recognizer.addModel(PathModel(directions: [4,2], datas:" " as AnyObject))
+        recognizer.addModel(PathModel(directions: [4,3,2,1,0,7,6,5,0], datas:"G" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2,6,7,0,1,2], datas:"H" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2], datas:"I" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2,3,4], datas:"J" as AnyObject))
+        recognizer.addModel(PathModel(directions: [3,4,5,6,7,0,1], datas:"K" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2,0], datas:"L" as AnyObject))
+        recognizer.addModel(PathModel(directions: [6,1,7,2], datas:"M" as AnyObject))
+        recognizer.addModel(PathModel(directions: [6,1,6], datas:"N" as AnyObject))
+        recognizer.addModel(PathModel(directions: [4,3,2,1,0,7,6,5,4], datas:"O" as AnyObject))
+        recognizer.addModel(PathModel(directions: [6,7,0,1,2,3,4], datas:"P" as AnyObject))
+        recognizer.addModel(PathModel(directions: [4,3,2,1,0,7,6,5,4,0], datas:"Q" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2,6,7,0,1,2,3,4,1], datas:"R" as AnyObject))
+        recognizer.addModel(PathModel(directions: [4,3,2,1,0,1,2,3,4], datas:"S" as AnyObject))
+        recognizer.addModel(PathModel(directions: [0,2], datas:"T" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2,1,0,7,6], datas:"U" as AnyObject))
+        recognizer.addModel(PathModel(directions: [1,7,0], datas:"V" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2,7,1,6], datas:"W" as AnyObject))
+        recognizer.addModel(PathModel(directions: [1,0,7,6,5,4,3], datas:"X" as AnyObject))
+        recognizer.addModel(PathModel(directions: [2,1,0,7,6,2,3,4,5,6,7], datas:"Y" as AnyObject))
+        recognizer.addModel(PathModel(directions: [0,3,0], datas:"Z" as AnyObject))
+        self.recognizer = recognizer
+        
+        super.viewDidLoad()
+        
+    }
+    
+    //takes the coordinates of the first touch
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        rawPoints = []
+//        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+
+        let touch = touches.first
+        let location = touch!.location(in: view)
+        rawPoints.append(Int(location.x))
+        rawPoints.append(Int(location.y))
+    }
+    
+    //takes all coordinates if touch moves and
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first
+        let location = touch!.location(in: view)
+        if(rawPoints[rawPoints.count-2] != Int(location.x) && rawPoints[rawPoints.count-1] != Int(location.y)) {
+            rawPoints.append(Int(location.x))
+            rawPoints.append(Int(location.y))
+        }
+        self.renderView.pointsToDraw = rawPoints
+    }
+    
+    //create the final path and makes action is letter is S
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        var path:Path = Path()
+        path.addPointFromRaw(rawPoints)
+        
+        let gesture:PathModel? = self.recognizer!.recognizePath(path)
+        
+        if gesture != nil {
+            let letters = gesture!.datas as? String
+            letterLabel.text  = letterLabel.text! + letters!
+        }
+        else
+        {
+//            letterLabel.text = ""
+        }
+
+    }
+    
+    func update(){
+        timer.invalidate()
+        rawPoints = []
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
